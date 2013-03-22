8a5f353548f8a93eb5226dd5368bda2c69b760b1
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-03-22 14:42:55 +0100

    Starts to look like something

---
+++ introduction.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('head.php'); ?>
+
+<body data-spy="scroll" data-target=".bs-docs-sidebar">
+
+	<?php include('navbar.php'); ?>
+
+<!-- Subhead
+================================================== -->
+<header class="jumbotron subhead" id="overview">
+  <div class="container">
+    <h1>Introduction</h1>
+    <p class="lead">ATLAS Wireless Safety System</p>
+  </div>
+</header>
+
+<?php include('footer.php'); ?>
+<?php include('script.php'); ?>
+
+</body>
+</html>
---
+++ people.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('head.php'); ?>
+
+<body data-spy="scroll" data-target=".bs-docs-sidebar">
+
+	<?php include('navbar.php'); ?>
+
+<!-- Subhead
+================================================== -->
+<header class="jumbotron subhead" id="overview">
+  <div class="container">
+    <h1>People</h1>
+    <p class="lead">People and Companies working on ATWSS</p>
+  </div>
+</header>
+
+  <div class="container">
+
+    <!-- Docs nav
+    ================================================== -->
+    <div class="row">
+      <div class="span3 bs-docs-sidebar">
+        <ul class="nav nav-list bs-docs-sidenav">
+          <li><a href="#santos"><i class="icon-chevron-right"></i>Alexandre Alves Dos Santos</a></li>
+          <li><a href="#beltramello"><i class="icon-chevron-right"></i>Olga Beltramello</a></li>
+          <li><a href="#donszelmann"><i class="icon-chevron-right"></i>Mark D&ouml;nszelmann</a></li>
+          <li><a href="#mladenov"><i class="icon-chevron-right"></i>Dimitar Mladenov</a></li>
+          <li><a href="#novocaptis"><i class="icon-chevron-right"></i>NOVOCAPTIS</a></li>
+          <li><a href="#prisma"><i class="icon-chevron-right"></i>PRISMA</a></li>
+        </ul>
+      </div>
+      <div class="span9">
+      
+        <section id="santos">
+          <div class="page-header">
+            <h1>Alexandre Alves Dos Santos</h1>
+          </div>
+          
+          
+      	</section>
+      	
+        <section id="beltramello">
+          <div class="page-header">
+            <h1>Olga Beltramello</h1>
+          </div>
+          
+          
+      	</section>
+      	
+        <section id="donszelmann">
+          <div class="page-header">
+            <h1>Mark D&ouml;nszelmann</h1>
+          </div>
+          
+          
+      	</section>
+      	
+        <section id="mladenov">
+          <div class="page-header">
+            <h1>Dimitar Mladenov</h1>
+          </div>
+          
+          
+      	</section>
+      	
+        <section id="novocaptis">
+          <div class="page-header">
+            <h1>NOVOCAPTIS</h1>
+          </div>
+          
+          
+      	</section>
+      	
+        <section id="prisma">
+          <div class="page-header">
+            <h1>PRISMA</h1>
+          </div>
+          
+          
+      	</section>
+      	
+            	
+      </div>
+    </div>
+
+  </div>
+  
+  <?php include('footer.php'); ?>
+  <?php include('script.php'); ?>
+  
+</body>
+</html>
