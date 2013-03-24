d31052c4b5f3ab9894bdf9dbb7fe8a359d754e1c
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-03-24 20:32:32 +0100

    Start using hogan to rebuild website

--- README.md
+++ README.md
@@ -3,4 +3,14 @@ ATWSS
 
 Atlas Wireless Safety System (website)
 
+Rebuild the website:
+
+- Make sure you have node.js and npm from nodejs.org
+- in build run 
+npm install
+
+- to rebuild from root dir run
+node build
+
+
 Mark Donszelmann
--- equipment.php
+++ equipment.php
@@ -22,17 +22,20 @@
     <div class="row">
       <div class="span3 bs-docs-sidebar">
         <ul class="nav nav-list bs-docs-sidenav">
+          <li><a href="#helmet"><i class="icon-chevron-right"></i>Helmet</a></li>
           <li><a href="#ptu"><i class="icon-chevron-right"></i>PTU</a></li>
+          <li><a href="#dosimeter"><i class="icon-chevron-right"></i>Dosimeter</a></li>
+          <li><a href="#person"><i class="icon-chevron-right"></i>Person</a></li>
         </ul>
       </div>
       <div class="span9">
       
-        <section id="ptu">
+        <section id="Person">
           <div class="page-header">
-            <h1>PTU</h1>
+            <h1>Person</h1>
           </div>
           
-          
+          <img src=""/>
       	</section>
       	            	
       </div>
