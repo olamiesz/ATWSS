d612cc866296455454afdd152365751cfe5ee108
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-05-29 10:13:13 +0200

    Added index, dosimeters

---
+++ docs/dosimeters.php
@@ -0,0 +1,10 @@
+---
+layout: default
+title: Dosimeters
+lead: Recorded dosimeter values of interventions in ATLAS
+slug: dosimeters
+---
+
+	<?php readfile('https://atlas.web.cern.ch/Atlas/TCOORD/CavCom/plot.php'.
+        (array_key_exists('InterventionID', $_GET) ? '?InterventionID='.$_GET['InterventionID'] : '')); 
+        ?>
---
+++ docs/index.php
@@ -0,0 +1,7 @@
+---
+layout: home
+title: ATWSS
+lead: A System to record and playback Interventions in the ATLAS cavern
+slug: atwss
+---
+
