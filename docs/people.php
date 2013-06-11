83e82a12544aca4ce177173f1545f196330c4be3
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-06-11 10:16:55 +0200

    Rely on updated bootstrap directory

--- docs/people.php
+++ docs/people.php
@@ -6,52 +6,46 @@ slug: people
 ---
 
     <div class="row">
-      <div class="span12">
- 		<ul class="thumbnails">
-		  <li class="span3">
+		  <div class="col-lg-3">
 		    <div class="thumbnail">
 		      <img data-src="holder.js/200x100" alt="" src="img/Alexandre.jpeg" width="150">
 		      <h3>Alexandre Dos Santos</h3>
 		      <p>is developer of the Audio and other parts of the Web Application software 
 		      and works on the hardware for ATWSS.</p>
 		    </div>
-		  </li>
+		  </div>
 		  
-		  <li class="span3">
+		  <div class="col-lg-3">
 		    <div class="thumbnail">
 		      <img data-src="holder.js/200x100" alt="" src="img/Olga.jpeg" width="150">
 		      <h3>Olga Beltramello</h3>
 		      <p>is the project leader of ATWSS and of the follow-up project 
 		      <a href="http://cern.ch/edusafe">EDUSAFE</a>. She is in charge of Safety in te ATLAS experiment.</p>
 		    </div>
-		  </li>
+		  </div>
 
-		  <li class="span3">
+		  <div class="col-lg-3">
 		    <div class="thumbnail">
 		      <img data-src="holder.js/200x100" alt="" src="img/Mark.jpeg" width="150">
 		      <h3>Mark D&ouml;nszelmann</h3>
 		      <p>is architect of the software of ATWSS and designer of the Web Application. 
 		      He is in charge of keeping the software running.</p>
 		    </div>
-		  </li>
+		  </div>
 		  
-		  <li class="span3">
+		  <div class="col-lg-3">
 		    <div class="thumbnail">
 		      <img data-src="holder.js/200x100" alt="" src="img/Dimi.jpeg" width="160">
 		      <h3>Dimitar Mladenov</h3>
 		      <p>is in charge of all the hardware on ATWSS. He also handles everything else we tend to forget...</p>
 		    </div>
-		  </li>
+		  </div>
 
-		</ul>
-	  </div>
     </div>
 
     <div class="row">
-      <div class="span12">
- 		<ul class="thumbnails">
 
-		  <li class="span3">
+		  <div class="col-lg-3">
 		    <div class="thumbnail">
 		      <img data-src="holder.js/200x100" alt="" src="img/CERN.jpeg" width="90">
 		      <h3>CERN</h3>
@@ -59,9 +53,9 @@ slug: people
 		      ATWSS was developed partially at CERN.</p><br/>
 		      <a href="http://www.cern.ch">www.cern.ch</a>
 		    </div>
-		  </li>
+		  </div>
 
-		  <li class="span3">
+		  <div class="col-lg-3">
 		    <div class="thumbnail">
 		      <img data-src="holder.js/200x100" alt="" src="img/ATLAS.png" width="250">
 		      <h3>ATLAS</h3>
@@ -69,28 +63,24 @@ slug: people
 		      we use ATWSS to record intreventions needed in the cavern.</p><br/>
 		      <a href="http://cern.ch/atlas">cern.ch/atlas</a>
 		    </div>
-		  </li>
+		  </div>
 
-		  <li class="span3">
+		  <div class="col-lg-3">
 		    <div class="thumbnail">
 		      <img data-src="holder.js/200x100" alt="" src="img/Novocaptis.png" width="178">
 		      <h3>Novocaptis</h3>
 		      <p>built the camera system as well as all the video handling in hardware and software.</p><br/>
 		      <a href="http://www.novocaptis.com">www.novocaptis.com</a>
 		    </div>
-		  </li>
+		  </div>
 
-		  <li class="span3">
+		  <div class="col-lg-3">
 		    <div class="thumbnail">
 		      <img data-src="holder.js/200x100" alt="" src="img/Prisma.png" width="192">
 		      <h3>Prisma</h3>
 		      <p>build the PTU unit and the software for all the measurements.</p><br/>
 		      <a href="http://www.prismaelectronics.eu">www.prismaelectronics.eu</a>
 		    </div>
-		  </li>
-		</ul>
-	</div>
-
-
+		  </div>
   </div>
   
