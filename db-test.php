76bb69b3ce31982621e5bcb588ba1caf636e0232
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-06-12 10:28:54 +0200

    Added test for db

---
+++ db-test.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('config/oracle.php');
+
+date_default_timezone_set('Europe/Berlin');
+
+$conn = oci_connect($conf['user'], $conf['password'], $conf['host']) or die(oci_error());
+
+$sql = oci_parse($conn, "select * from view_interventions");
+oci_execute($sql) or die(oci_error($sql));
+$nrows = oci_fetch_all($sql, $intervention);
+
+echo "# of rows ".$nrows."\n";
+
+oci_free_statement($sql);
+oci_close($conn);
+
+?>
